f26ff930d9f2f66b286f556186e8ea45793655bd
Fidel Yin <fidel.yin@hotmail.com>
2025-07-10 18:23:12 +0800

    nvim: revert defaults to install the latest stable version of plugins

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -1,5 +1,6 @@
 return {
     "saghen/blink.cmp",
+    version = "*", -- stable version required to download the corresponding binary built
     -- optional: provides snippets for the snippet source
     dependencies = {
         "rafamadriz/friendly-snippets",
