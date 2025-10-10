ade064b0c9195356bb9f8de13b3b440bab5a04d3
Fidel Yin <fidel.yin@hotmail.com>
2025-10-10 16:42:45 -0400

    nvim: fix for vscode

--- .config/nvim/lua/config/vscode.lua
+++ .config/nvim/lua/config/vscode.lua
@@ -1,6 +1,11 @@
+if not vim.g.vscode then
+    return {}
+end
+
 local enabled_plugins = {
     "flash.nvim",
     "nvim-toggler",
+    "nvim-treesitter",
     "nvim-treesitter-textobjects",
 }
 
@@ -10,3 +15,5 @@ config.options.change_detection.enabled = false
 config.options.defaults.cond = function(plugin)
     return vim.tbl_contains(enabled_plugins, plugin.name)
 end
+
+return {}
