2194e314b6acc82faac7cb739f32642e723adb81
Fidel Yin <fidel.yin@hotmail.com>
2025-11-05 20:06:07 -0500

    nvim: fix guess-indent for tab indent

--- .config/nvim/lua/plugins/guess-indent.lua
+++ .config/nvim/lua/plugins/guess-indent.lua
@@ -2,5 +2,11 @@ return {
     "nmac427/guess-indent.nvim",
     event = "BufRead",
 
-    opts = {},
+    opts = {
+        on_tab_options = {
+            ["expandtab"] = false,
+            ["softtabstop"] = 0,
+            ["shiftwidth"] = 0,
+        },
+    },
 }
