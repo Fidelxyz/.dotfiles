f9b720eb4d9be8f8824ce49de117a6192bff088c
Fidel Yin <fidel.yin@hotmail.com>
2025-12-03 18:22:55 -0500

    nvim: add keymaps to enable and disable formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
     "stevearc/conform.nvim",
 
     event = { "BufWritePre" },
-    cmd = { "ConformInfo", "Format" },
+    cmd = { "ConformInfo", "Format", "FormatDisable", "FormatEnable" },
 
     opts = {
         formatters_by_ft = {
@@ -30,5 +30,33 @@ return {
             end
             conform.format({ async = true, lsp_format = "fallback", range = range })
         end, { range = true })
+
+        require("conform").setup({
+            format_on_save = function(bufnr)
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_format = "fallback" }
+            end,
+        })
+
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+            if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = "Disable autoformat-on-save",
+            bang = true,
+        })
+        vim.api.nvim_create_user_command("FormatEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, {
+            desc = "Re-enable autoformat-on-save",
+        })
     end,
 }
