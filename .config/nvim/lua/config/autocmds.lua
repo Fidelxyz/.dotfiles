6d2aacb9688837e8e2a1ae6a10ecfdb6b11b618e
Fidel Yin <fidel.yin@hotmail.com>
2025-12-03 18:23:18 -0500

    nvim: add autocmds to auto load and store views

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,6 @@
 local utils = require("utils")
 
 vim.api.nvim_create_augroup("CursorLine", {})
-
 vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
     group = "CursorLine",
     callback = function()
@@ -11,7 +10,6 @@ vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
     end,
     desc = "Enable cursorline when entering a window",
 })
-
 vim.api.nvim_create_autocmd({ "WinLeave" }, {
     group = "CursorLine",
     callback = function()
@@ -23,7 +21,6 @@ vim.api.nvim_create_autocmd({ "WinLeave" }, {
 })
 
 vim.api.nvim_create_augroup("SpellCheck", {})
-
 vim.api.nvim_create_autocmd({ "FileType" }, {
     group = "SpellCheck",
     pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
@@ -33,3 +30,15 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end,
     desc = "Enable spellcheck for defined filetypes",
 })
+
+-- Auto save and load view
+vim.api.nvim_create_augroup("AutoView", {})
+vim.api.nvim_create_autocmd("BufWinLeave", {
+    group = "AutoView",
+    command = "silent! mkview",
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = "AutoView",
+    command = "silent! loadview",
+})
+vim.opt.viewoptions = "cursor,folds"
