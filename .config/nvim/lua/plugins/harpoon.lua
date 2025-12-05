76f6bb0ba863f661004fb11d57f02ecd90e7c3dd
Fidel Yin <fidel.yin@hotmail.com>
2025-12-05 18:52:20 -0500

    nvim: edit harpoon keymaps

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -6,25 +6,23 @@ return {
     -- stylua: ignore
     keys = {
         {
-            "<Tab>",
+            "<leader>hh",
             function()
                 local harpoon = require("harpoon")
                 harpoon.ui:toggle_quick_menu(harpoon:list())
             end,
             desc = "Harpoon",
         },
-        { "<leader>a", function() require("harpoon"):list():add() end,     desc = "Harpoon: Add to list" },
-        { "1",         function() require("harpoon"):list():select(1) end, desc = "Harpoon: Select list item 1" },
-        { "2",         function() require("harpoon"):list():select(2) end, desc = "Harpoon: Select list item 2" },
-        { "3",         function() require("harpoon"):list():select(3) end, desc = "Harpoon: Select list item 3" },
-        { "4",         function() require("harpoon"):list():select(4) end, desc = "Harpoon: Select list item 4" },
-        { "5",         function() require("harpoon"):list():select(5) end, desc = "Harpoon: Select list item 5" },
-        { "6",         function() require("harpoon"):list():select(6) end, desc = "Harpoon: Select list item 6" },
-        { "7",         function() require("harpoon"):list():select(7) end, desc = "Harpoon: Select list item 7" },
-        { "8",         function() require("harpoon"):list():select(8) end, desc = "Harpoon: Select list item 8" },
-        { "9",         function() require("harpoon"):list():select(9) end, desc = "Harpoon: Select list item 9" },
-        { "<leader>]", function() require("harpoon"):list():next() end,    desc = "Harpoon: Next list item" },
-        { "<leader>[", function() require("harpoon"):list():prev() end,    desc = "Harpoon: Previous list item" },
+        { "<leader>ha", function() require("harpoon"):list():add() end,     desc = "Harpoon: Add to list" },
+        { "1",          function() require("harpoon"):list():select(1) end, desc = "Harpoon: Select list item 1" },
+        { "2",          function() require("harpoon"):list():select(2) end, desc = "Harpoon: Select list item 2" },
+        { "3",          function() require("harpoon"):list():select(3) end, desc = "Harpoon: Select list item 3" },
+        { "4",          function() require("harpoon"):list():select(4) end, desc = "Harpoon: Select list item 4" },
+        { "5",          function() require("harpoon"):list():select(5) end, desc = "Harpoon: Select list item 5" },
+        { "6",          function() require("harpoon"):list():select(6) end, desc = "Harpoon: Select list item 6" },
+        { "7",          function() require("harpoon"):list():select(7) end, desc = "Harpoon: Select list item 7" },
+        { "8",          function() require("harpoon"):list():select(8) end, desc = "Harpoon: Select list item 8" },
+        { "9",          function() require("harpoon"):list():select(9) end, desc = "Harpoon: Select list item 9" },
     },
 
     opts = {
