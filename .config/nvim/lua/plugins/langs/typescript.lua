a4867b814c933af3173776010f8a2aa5595479cc
Fidel Yin <fidel.yin@hotmail.com>
2026-01-19 14:11:34 -0500

    nvim: enable close on slash for nvim-ts-autotag

--- .config/nvim/lua/plugins/langs/typescript.lua
+++ .config/nvim/lua/plugins/langs/typescript.lua
@@ -111,6 +111,8 @@ return {
         "windwp/nvim-ts-autotag",
         event = { "BufReadPre", "BufNewFile" },
 
-        opts = {},
+        opts = {
+            enable_close_on_slash = true,
+        },
     },
 }
