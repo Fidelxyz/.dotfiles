d6013309085be3bee8c03d9e6dac8fcfa5954e92
Fidel Yin <fidel.yin@hotmail.com>
2026-02-27 09:01:25 -0500

    nvim: show path only on dropbar

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -22,14 +22,11 @@ return {
                 "BufRead",
                 "BufNewFile",
             },
+            -- Show path only
+            sources = function()
+                local sources = require("dropbar.sources")
+                return { sources.path }
+            end,
         },
     },
-    config = function(_, opts)
-        require("dropbar").setup(opts)
-
-        local dropbar_api = require("dropbar.api")
-        vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-        vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-        vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-    end,
 }
